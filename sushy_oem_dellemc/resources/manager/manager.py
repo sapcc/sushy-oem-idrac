9bc87a020daf724ed6bfd613b4438b1863c6dff6
Ilya Etingof <etingof@gmail.com>
2019-08-16 11:40:54 +0200

    Accommodate changed sushy OEM architecture

--- sushy_oem_dellemc/resources/manager/manager.py
+++ sushy_oem_dellemc/resources/manager/manager.py
@@ -12,29 +12,117 @@
 
 import logging
 
+import sushy
+from sushy.resources import base
+from sushy.resources import common
 from sushy.resources.oem import base as oem_base
+
+import eventlet
 
 LOG = logging.getLogger(__name__)
 
 
-class DellManagerActionsField(oem_base.OEMActionsField):
-    import_system_configuration = oem_base.OEMActionField(
+class DellManagerActionsField(base.CompositeField):
+    import_system_configuration = common.ActionField(
         lambda key, **kwargs: key.endswith(
             '#OemManager.ImportSystemConfiguration'))
 
 
-class DellManagerExtension(oem_base.OEMExtensionResourceBase):
+class DellManagerExtension(oem_base.OEMResourceBase):
 
     _actions = DellManagerActionsField('Actions')
 
-    def __init__(self, resource, *args, **kwargs):
-        """A class representing Manager OEM extension for Dell
+    MAGIC_CD_SAUCE = """\
+    <SystemConfiguration>\
+      <Component FQDD="iDRAC.Embedded.1">\
+        <Attribute Name="ServerBoot.1#BootOnce">\
+          Enabled\
+        </Attribute>\
+        <Attribute Name="ServerBoot.1#FirstBootDevice">\
+          VCD-DVD\
+        </Attribute>\
+      </Component>\
+    </SystemConfiguration>\
+    """
 
-        :param resource: The parent Manager resource instance
-        """
-        super(DellManagerExtension, self).__init__(
-            resource, 'Dell', *args, **kwargs)
+    MAGIC_FLOPPY_SAUCE = """\
+        <SystemConfiguration>\
+          <Component FQDD="iDRAC.Embedded.1">\
+            <Attribute Name="ServerBoot.1#BootOnce">\
+              Enabled\
+            </Attribute>\
+            <Attribute Name="ServerBoot.1#FirstBootDevice">\
+              VFDD\
+            </Attribute>\
+          </Component>\
+        </SystemConfiguration>\
+    """
+
+    MAGIC_SAUCER = {
+        sushy.VIRTUAL_MEDIA_FLOPPY: MAGIC_FLOPPY_SAUCE,
+        sushy.VIRTUAL_MEDIA_CD: MAGIC_CD_SAUCE
+    }
 
     @property
     def import_system_configuration_uri(self):
         return self._actions.import_system_configuration.target_uri
+
+    def set_virtual_boot_device(cls, device, persistent=False):
+        """Set boot device for a node.
+
+        Dell iDRAC Redfish implementation does not support setting
+        boot device to virtual media via standard Redfish means.
+        However, this still can be done via an OEM extension.
+
+        :param task: a TaskManager instance.
+        :param device: Boot device. Values are vendor-specific.
+        :param persistent: Whether to set next-boot, or make the change
+            permanent. Default: False.
+        :raises: InvalidParameterValue if the validation of the
+            ManagementInterface fails.
+        """
+        try:
+            magic_saucer = cls.MAGIC_SAUCER[device]
+
+        except KeyError:
+            raise sushy.exceptions.InvalidParameterValue(
+                'Unknown or unsupported device %s' % device)
+
+        system = sushy.get_system(task.node)
+
+        for manager in system.managers:
+
+            # NOTE (etingof): this call should make sushy go fishing for
+            # sushy extensions installed on the system. Then find one
+            # for 'Dell' implementing 'Manager' resource extension and return
+            # it here.
+            manager_oem = manager.get_oem_extension('Dell')
+
+            if not manager_oem:
+                continue
+
+            manager_oem.import_system_configuration(
+                shared_configuration={'Target': 'ALL'},
+                import_buffer=magic_saucer)
+
+            # TODO (etingof): figure out if on-time or persistent boot can at
+            # all be implemented via this OEM call
+
+            LOG.info("Set node %(node) boot device to %(device) via "
+                     "OEM magic", {'node': task.node.uuid,
+                                   'device': device})
+            break
+
+        else:
+            raise sushy.exceptions.InvalidParameterValue(
+                driver='redfish',
+                reason=_('Dell OEM manager resources not found. Make sure you '
+                         'have sushy Dell OEM extension package installed.'))
+
+        # TODO(etingof): we need to poll Redfish TaskService instead,
+        # but that's not yet implemented in sushy
+        eventlet.sleep(30)
+
+
+def get_extension(*args, **kwargs):
+    return DellManagerExtension
